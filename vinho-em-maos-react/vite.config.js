a58a6ddb55245a400b6b70a6693412842ed3cb7d
celso luiz <celsot.holistics@gmail.com>
2026-06-18 10:22:00 -0300

    fix vite package

--- vinho-em-maos-react/vite.config.js
+++ vinho-em-maos-react/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-oxc'
 
 export default defineConfig({
   plugins: [react()],
@@ -11,9 +11,5 @@ export default defineConfig({
         changeOrigin: true
       }
     }
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: true
   }
-});
+})
